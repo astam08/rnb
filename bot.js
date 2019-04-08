d222f795fd3100fcdb9ad7ea2fb0c8f158415a61
astam08 <49343371+astam08@users.noreply.github.com>
2019-04-08 07:55:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js") 
 const settings = require("./your_settings.json")
-const antispam = require("./antispam.js")
 const bot = new Discord.Client()
 bot.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
